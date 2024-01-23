9dd1afb53550047743a59422b8498fa5f9bc9b54
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-01-23 11:41:42 -0700

    Ejemplo de declaraciones múltiples

--- Ejemplo/Ejemplo/PlaygroundBases.playground/Contents.swift
+++ Ejemplo/Ejemplo/PlaygroundBases.playground/Contents.swift
@@ -8,4 +8,14 @@ var greeting = "Hello, playground"
  Comentario
  de varias
  lineas
- */ﬂﬂ
+ */
+
+// - Variables y constantes
+
+var str = "Hello world"
+str = "Hola mundo"
+
+let constante = "Texto constante"
+// constante = "Valor cambiado" // Error: Una constante no puede ser actualizada
+
+let nombreCompleto = "Juan Perez", edad = 23, peso = 70.5, lugarNacimiento = "Mexico"
