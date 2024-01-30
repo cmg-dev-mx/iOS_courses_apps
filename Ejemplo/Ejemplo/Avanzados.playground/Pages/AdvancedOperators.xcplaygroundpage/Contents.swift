6bad5d8dbd50f6e624435c233af1a9292458e5b3
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-01-30 14:20:03 -0700

    Ejemplo de precedencia y asociatividad

--- Ejemplo/Ejemplo/Avanzados.playground/Pages/AdvancedOperators.xcplaygroundpage/Contents.swift
+++ Ejemplo/Ejemplo/Avanzados.playground/Pages/AdvancedOperators.xcplaygroundpage/Contents.swift
@@ -1,8 +1,13 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
+// Precedencia y asociatividad
 
-//: [Next](@next)
- 
+let myInt: Int = 2 + 3 % 4 * 5 // 2 + ((3 % 4) * 5) = 17
+let myInt2: Int = (((2 + 3) % 4) * 5) // 5
+
+// Más operadores avanzados
+
+// - Operadores bit a bit: Realizar operaciones con bits de tipo NOT, AND, OR XOR
+// - Operadores de desplazamiento: Desplazar bits hacia la izquierda o derecha
+// - Operadores de desbordamiento: Manejar errores en valores por encima de su límite
+// - Operadores custom: Definir operadores personalizados
