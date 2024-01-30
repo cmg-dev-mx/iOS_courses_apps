c60025226dbb849b4542a77ca01143aba2027466
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-01-30 15:06:35 -0700

    Ejemplo de ordenamiento con sorted

--- Ejemplo/Ejemplo/ColeccionesAvanzados.playground/Pages/CollectionAlgorithms.xcplaygroundpage/Contents.swift
+++ Ejemplo/Ejemplo/ColeccionesAvanzados.playground/Pages/CollectionAlgorithms.xcplaygroundpage/Contents.swift
@@ -20,16 +20,40 @@ let myDictionary = [
 let mySet: Set = [5, 8, 1, 0, 3, 9, 7, 2, 4, 6]
 
 // Sort
-print(myArray)
-myArray.sort()
-print(myArray)
+// print(myArray)
+// myArray.sort()
+// print(myArray)
 
-myArray.sort { (a, b) -> Bool in
-    return a > b
-}
-print(myArray)
+// myArray.sort { (a, b) -> Bool in
+//     return a > b
+// }
+// print(myArray)
 
 // MyDictionary.sort() No soportada
 // mySet.sort() No soportada
 
+// Sorted
 
+print(myArray)
+var myArraySorted = myArray.sorted() // No modifica el arreglo original
+print(myArray)
+print(myArraySorted)
+
+myArraySorted = myArray.sorted { (a, b) -> Bool in
+    return a > b
+}
+print(myArray)
+print(myArraySorted)
+
+let myDictionarySorted = myDictionary.sorted { (a, b) -> Bool in
+    return a.key < b.key
+}
+print(myDictionarySorted)
+
+var mySetSorted = mySet.sorted()
+print(mySetSorted)
+
+mySetSorted = mySet.sorted { (a, b) -> Bool in
+    return a > b
+}
+print(mySetSorted)
