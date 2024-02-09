ebd5336584f72dcedb95d7512505ae1c404861df
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-02-09 14:04:33 -0700

    Creación de app base

---
+++ Controles y vistas de texto/Controles y vistas de texto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Controles y vistas de texto
+//
+//  Created by Cesar Garduno on 2/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
