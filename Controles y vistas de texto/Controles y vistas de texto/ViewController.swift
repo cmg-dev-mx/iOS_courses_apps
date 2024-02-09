9b7ed84e3c36ac0e97f8586bab608458f1a24bab
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-02-09 14:38:20 -0700

    Ejemplo de picker

--- Controles y vistas de texto/Controles y vistas de texto/ViewController.swift
+++ Controles y vistas de texto/Controles y vistas de texto/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 class ViewController: UIViewController {
     
     // Outlets
-    
     @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var myPickerView: UIPickerView!
+    
+    // Variables
+    private let myPickerViewValues = [
+        "Uno",
+        "Dos",
+        "Tres",
+        "Cuatro",
+        "Cinco"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +29,11 @@ class ViewController: UIViewController {
         myButton.setTitle("Mi botón", for: .normal)
         myButton.backgroundColor = .blue
         myButton.setTitleColor(.white, for: .normal)
+        
+        // Pickers
+        myPickerView.backgroundColor = .lightGray
+        myPickerView.dataSource = self
+        myPickerView.delegate = self
     }
     
     // Actions
@@ -35,3 +49,22 @@ class ViewController: UIViewController {
     
 }
 
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return myPickerViewValues.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return myPickerViewValues[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let myString = myPickerViewValues[row]
+        myButton.setTitle(myString, for: .normal)
+    }
+}
