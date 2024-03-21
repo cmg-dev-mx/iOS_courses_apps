f34fd1864f5eaf73cc38fb48493e94613e2ca924
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-03-20 21:39:09 -0600

    Ejemplo de StackView (Linear layout de iOS)

--- ViewsAndNavigation/ViewsAndNavigation/ViewController.swift
+++ ViewsAndNavigation/ViewsAndNavigation/ViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var myFakeView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func myButtonAction(_ sender: Any) {
+//        myView.isHidden = true
+        myFakeView.isHidden = true
+    }
 }
-
