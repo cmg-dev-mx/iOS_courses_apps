311abe35d227054a2d57c140118d03d8fc95a837
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-04-27 11:40:19 -0600

    Creación de aplicación base

---
+++ TablasColecciones/TablasColecciones/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TablasColecciones
+//
+//  Created by Cesar Morales Garduño on 4/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
