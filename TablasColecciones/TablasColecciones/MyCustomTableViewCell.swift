ba0d5836cb82a2f40e50870187838ecf297d8a62
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-04-27 13:03:03 -0600

    Implementación de celda customizada

---
+++ TablasColecciones/TablasColecciones/MyCustomTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MyCustomTableViewCell.swift
+//  TablasColecciones
+//
+//  Created by Cesar Morales Garduño on 4/27/24.
+//
+
+import UIKit
+
+class MyCustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var myFirstLabel: UILabel!
+    @IBOutlet weak var mySecondLabel: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        myFirstLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        myFirstLabel.textColor = .blue
+
+        backgroundColor = .gray
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        print(myFirstLabel.text ?? "")
+    }
+}
--- TablasColecciones/TablasColecciones/ViewController.swift
+++ TablasColecciones/TablasColecciones/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
         myTableView.dataSource = self
         myTableView.delegate = self
         myTableView.tableFooterView = UIView()
+
+        myTableView.register(UINib(nibName: "MyCustomTableViewCell", bundle: nil), forCellReuseIdentifier: "myCustomCell")
     }
 }
 
@@ -27,20 +29,37 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return myContries.count
     }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 {
+            return 50
+        } else {
+            return 150
+        }
+    }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: "myCell")
-        if cell == nil {
-            cell = UITableViewCell(style: .default, reuseIdentifier: "myCell")
-            cell?.backgroundColor = .gray
-            cell?.textLabel?.font = UIFont.systemFont(ofSize: 20)
-            cell?.accessoryType = .disclosureIndicator
+        if indexPath.section == 0 {
+            var cell = tableView.dequeueReusableCell(withIdentifier: "myCell")
+            if cell == nil {
+                cell = UITableViewCell(style: .default, reuseIdentifier: "myCell")
+                cell?.backgroundColor = .gray
+                cell?.textLabel?.font = UIFont.systemFont(ofSize: 20)
+                cell?.accessoryType = .disclosureIndicator
+            }
 
-
+            cell!.textLabel?.text = myContries[indexPath.row]
+            return cell!
+        } else {
+            let cell = myTableView.dequeueReusableCell(withIdentifier: "myCustomCell", for: indexPath) as? MyCustomTableViewCell
+            cell?.myFirstLabel.text = String(indexPath.row + 1)
+            cell?.mySecondLabel.text = myContries[indexPath.row]
+            return cell!
         }
-
-        cell!.textLabel?.text = myContries[indexPath.row]
-        return cell!
     }
 }
 
