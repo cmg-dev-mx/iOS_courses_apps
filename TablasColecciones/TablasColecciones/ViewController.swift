d3a12930d0fbe04d91992927f47f32a913181a0b
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-04-27 12:28:53 -0600

    Implementación de tabla

--- TablasColecciones/TablasColecciones/ViewController.swift
+++ TablasColecciones/TablasColecciones/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTableView: UITableView!
+
+    private let myContries = ["Mexico", "USA", "Canada", "Brazil", "Argentina", "Chile", "Peru", "Colombia", "Venezuela", "Ecuador", "Bolivia", "Paraguay", "Uruguay"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        myTableView.dataSource = self
+        myTableView.tableFooterView = UIView()
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myContries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "myCell")
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "myCell")
+            cell?.backgroundColor = .gray
+            cell?.textLabel?.font = UIFont.systemFont(ofSize: 20)
+        }
+
+        cell!.textLabel?.text = myContries[indexPath.row]
+        return cell!
+    }
+}
