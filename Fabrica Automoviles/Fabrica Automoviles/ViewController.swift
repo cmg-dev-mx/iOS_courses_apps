998ad639c821bf3d2eadd7d6fcd92ed4d11c1e2f
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-04-30 21:46:20 -0600

    Creación de proyecto Fabrica Automoviles

---
+++ Fabrica Automoviles/Fabrica Automoviles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fabrica Automoviles
+//
+//  Created by Cesar Morales Garduño on 4/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
