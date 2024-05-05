74d4d3c7be5d8d66b94834cfd862f31959327432
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-05-05 10:01:49 -0600

    Creación de app Lifeycle

---
+++ Lifecycle/Lifecycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lifecycle
+//
+//  Created by Cesar Morales Garduño on 5/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
