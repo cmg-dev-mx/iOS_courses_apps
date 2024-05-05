e35c942253aa8a0d9f62ec94aa7b73d1f77753d8
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-05-05 11:17:45 -0600

    Mapeo en logs de los distintos estados de VC, escenas y aplicación

--- Lifecycle/Lifecycle/SecondaryViewController.swift
+++ Lifecycle/Lifecycle/SecondaryViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondaryViewController.swift
 //  Lifecycle
 //
 //  Created by Cesar Morales Garduño on 5/5/24.
@@ -7,30 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SecondaryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Estoy en VC1 y se activo -> \(#function)")
+        print("Estoy en VC2 y se activo -> \(#function)")
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("Estoy en VC1 y se activo -> \(#function)")
+        print("Estoy en VC2 y se activo -> \(#function)")
     }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print("Estoy en VC1 y se activo -> \(#function)")
+        print("Estoy en VC2 y se activo -> \(#function)")
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        print("Estoy en VC1 y se activo -> \(#function)")
+        print("Estoy en VC2 y se activo -> \(#function)")
     }
 
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        print("Estoy en VC1 y se activo -> \(#function)")
+        print("Estoy en VC2 y se activo -> \(#function)")
     }
 }
