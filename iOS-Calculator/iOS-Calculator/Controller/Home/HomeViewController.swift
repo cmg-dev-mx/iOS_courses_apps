be9880cf378cf015756735c21f2c0626db5e657d
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-05-19 19:57:57 -0600

    Adición de conexión de elementos visuales en controller

--- iOS-Calculator/iOS-Calculator/Controller/Home/HomeViewController.swift
+++ iOS-Calculator/iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -8,7 +8,32 @@
 import UIKit
 
 final class HomeViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var number0: UIButton!
+    @IBOutlet weak var number1: UIButton!
+    @IBOutlet weak var number2: UIButton!
+    @IBOutlet weak var number3: UIButton!
+    @IBOutlet weak var number4: UIButton!
+    @IBOutlet weak var number5: UIButton!
+    @IBOutlet weak var number6: UIButton!
+    @IBOutlet weak var number7: UIButton!
+    @IBOutlet weak var number8: UIButton!
+    @IBOutlet weak var number9: UIButton!
+    @IBOutlet weak var comma: UIButton!
+    
+    @IBOutlet weak var addition: UIButton!
+    @IBOutlet weak var substraction: UIButton!
+    @IBOutlet weak var multiplication: UIButton!
+    @IBOutlet weak var division: UIButton!
+    @IBOutlet weak var percentage: UIButton!
+    @IBOutlet weak var toggleSign: UIButton!
+    @IBOutlet weak var equals: UIButton!
+    @IBOutlet weak var clean: UIButton!
+    
     // MARK: - Initialization
     
     init() {
