d06e07b536206808ccc8bbd1f6414694978875e6
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-05-19 19:17:36 -0600

    Creación de la vista principal

--- iOS-Calculator/iOS-Calculator/AppDelegate.swift
+++ iOS-Calculator/iOS-Calculator/AppDelegate.swift
@@ -13,7 +13,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        setupView()
         return true
     }
+    
+    // MARK: - Private methods
+    
+    private func setupView() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }    
 }
---
+++ iOS-Calculator/iOS-Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeViewController.swift
+//  iOS-Calculator
+//
+//  Created by Cesar Morales Garduño on 5/19/24.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    // MARK: - Initialization
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
