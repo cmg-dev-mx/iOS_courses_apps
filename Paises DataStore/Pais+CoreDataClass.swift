1351f8f38324b2196712a5d0a280053d92a13041
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-05-23 21:05:07 -0600

    Recuperación de datos directamente de Core Data

---
+++ Paises DataStore/Pais+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Pais+CoreDataClass.swift
+//  Paises DataStore
+//
+//  Created by Cesar Morales Garduño on 5/23/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Pais)
+public class Pais: NSManagedObject {
+
+}
--- Paises DataStore/Paises DataStore/ViewController.swift
+++ Paises DataStore/Paises DataStore/ViewController.swift
@@ -6,28 +6,46 @@
 //
 
 import UIKit
+// 1. Importar la librería Core Data
+import CoreData
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var myTableView: UITableView!
 
-    private let myCountries = ["Mexico", "USA", "Canada", "Brazil", "Argentina", "Chile", "Colombia", "Peru", "Ecuador", "Bolivia", "Uruguay", "Paraguay", "Venezuela", "Guatemala", "Honduras", "El Salvador", "Nicaragua", "Costa Rica", "Panama", "Cuba", "Puerto Rico", "Republica Dominicana", "Haiti", "Jamaica", "Trinidad y Tobago", "Barbados", "Bahamas", "Belice", "Guyana", "Surinam", "Guyana Francesa"]
+    // 2. Referencia al managed object context
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    // 3. Cambiar a variable de tipo pais sin datos iniciales
+    private var myCountries:[Pais]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         myTableView.dataSource = self
         myTableView.delegate = self
+
+        // 4. Recuperar datos
+        recuperarDatos()
     }
     
     @IBAction func addBtnAction(_ sender: Any) {
         print("Añadir Datos")
     }
+
+    private func recuperarDatos() {
+        do {
+            self.myCountries = try context.fetch(Pais.fetchRequest())
+            myTableView.reloadData()
+        } catch {
+            print("Error recuperando datos")
+        }
+    }
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return myCountries.count
+        return myCountries!.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -36,13 +54,13 @@ extension ViewController: UITableViewDataSource {
             cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
             cell?.textLabel?.font = UIFont.systemFont(ofSize: 20)   
         }
-        cell!.textLabel?.text = myCountries[indexPath.row]
+        cell!.textLabel?.text = myCountries![indexPath.row].nombre
         return cell!
     }
 }
 
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("Pais: \(myCountries[indexPath.row])")
+        print("Pais: \(myCountries![indexPath.row])")
     }
 }
