d3d23d857c33b9c853a399d2d5cb98e8884fd58b
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-05-23 20:19:59 -0600

    Creación de proyecto paises datastore

---
+++ Paises DataStore/Paises DataStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Paises DataStore
+//
+//  Created by Cesar Morales Garduño on 5/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
