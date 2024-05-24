a63711e726870cd03d4bc60a2e40c8a33f320f1b
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-05-23 19:27:34 -0600

    Creación de proyecto

---
+++ App Persistencia/App Persistencia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App Persistencia
+//
+//  Created by Cesar Morales Garduño on 5/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
