20f9d2007c044b49ef2305c339bb8ff309d335dc
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-05-23 19:55:21 -0600

    Creación de proyecto

---
+++ App Persistencia Core Data/App Persistencia Core Data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App Persistencia Core Data
+//
+//  Created by Cesar Morales Garduño on 5/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
