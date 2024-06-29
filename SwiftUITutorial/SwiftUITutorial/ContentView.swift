4e892b4e86d150a691936e0d322cceb50e0749bc
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-06-29 14:00:07 -0600

    Contenedores lineales (stacks)

--- SwiftUITutorial/SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/SwiftUITutorial/ContentView.swift
@@ -9,12 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        VStack( // Solamente permite 10 elementos internos
+            alignment: .leading,
+            spacing: 20
+        ) {
             Text("Hello, world!")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.gray)
+            
+            Spacer()
+
+            ZStack {
+                Text("")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.gray)
+                    .padding()
+                Text("")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.blue)
+                    .padding(40)
+                Text("")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.red)
+                    .padding(60)
+                Text("")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(Color.green)
+                    .padding(80)
+            }
+
+            Spacer()
+
+            HStack {
+            
+            Text("Saludos a todos!")
+                .padding()
+                .background(Color.gray)
+
+            Spacer()
+
+            Text("Hola")
+                .padding()
+                .background(Color.gray)
+            }
         }
+        .background(Color.yellow)
         .padding()
     }
 }
