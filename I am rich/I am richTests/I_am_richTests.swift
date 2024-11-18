8b5accdf891a112ac21f7a8a1a3489f988024434
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-11-18 13:24:51 -0700

    Creación del proyecto

---
+++ I am rich/I am richTests/I_am_richTests.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_richTests.swift
+//  I am richTests
+//
+//  Created by Cesar Garduno on 11/18/24.
+//
+
+import Testing
+@testable import I_am_rich
+
+struct I_am_richTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
