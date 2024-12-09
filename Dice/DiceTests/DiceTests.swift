ec53ccd41c83497c1d5c99b9acd6f37f2fad1ed5
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-12-09 15:01:50 -0700

    Creación del proyecto

---
+++ Dice/DiceTests/DiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  DiceTests.swift
+//  DiceTests
+//
+//  Created by Cesar Garduno on 12/9/24.
+//
+
+import Testing
+@testable import Dice
+
+struct DiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
