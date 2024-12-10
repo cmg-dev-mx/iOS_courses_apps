c0060c0ea723f816a07f17d0fef44b0096fe7f96
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-12-10 12:59:25 -0700

    Implementación de funcionalidad y vista

--- Dice/Dice/ViewController.swift
+++ Dice/Dice/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imgDiceLeft: UIImageView!
+    @IBOutlet weak var imgDiceRight: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imgDiceLeft.image = UIImage(named: "dice_1")
+        imgDiceRight.image = UIImage(named: "dice_1")
     }
 
-
+    @IBAction func noClickBtnThrow(_ sender: Any) {
+        let leftDice = Int.random(in: 1...6)
+        let rightDice = Int.random(in: 1...6)
+        
+        imgDiceLeft.image = UIImage(named: "dice_\(leftDice)")
+        imgDiceRight.image = UIImage(named: "dice_\(rightDice)")
+    }
 }
 
