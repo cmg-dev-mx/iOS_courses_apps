2fab8e4f246c52b18ee78438d326c8a5ec489297
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-12-11 08:51:54 -0700

    Creación del proyecto

---
+++ Xilophone/Xilophone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xilophone
+//
+//  Created by Cesar Garduno on 12/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Xilophone/XilophoneTests/XilophoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  XilophoneTests.swift
+//  XilophoneTests
+//
+//  Created by Cesar Garduno on 12/11/24.
+//
+
+import Testing
+@testable import Xilophone
+
+struct XilophoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
