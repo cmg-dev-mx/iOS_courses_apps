314b96878c0024871f045a1e9f441dbf07c287e0
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-12-11 11:18:53 -0700

    Adición de audios

--- Xilophone/Xilophone/ViewController.swift
+++ Xilophone/Xilophone/ViewController.swift
@@ -6,32 +6,47 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func onClickButtonC(_ sender: Any) {
+        playAudio(note: "C")
     }
     
     @IBAction func onClickButtonD(_ sender: Any) {
+        playAudio(note: "D")
     }
     
     @IBAction func onClickButtonE(_ sender: Any) {
+        playAudio(note: "E")
     }
     
     @IBAction func onClickButtonF(_ sender: Any) {
+        playAudio(note: "F")
     }
     
     @IBAction func onClickButtonG(_ sender: Any) {
+        playAudio(note: "G")
     }
     
     @IBAction func onClickButtonA(_ sender: Any) {
+        playAudio(note: "A")
     }
     
     @IBAction func onClickButtonB(_ sender: Any) {
+        playAudio(note: "B")
+    }
+    
+    private func playAudio(note: String) {
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
     }
 }
