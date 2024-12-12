1b7438ac11ed1711d6ac03bf62e3a8392b58252a
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-12-12 14:05:48 -0700

    Implementación de vista y funcionalidad básica

--- Egg Timer/Egg Timer/ViewController.swift
+++ Egg Timer/Egg Timer/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let softTime = 5
+    let mediumTime = 8
+    let hardTime = 12
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func onClickHardnessSelected(_ sender: UIButton) {
+        let hardness = sender.titleLabel?.text ?? "No title"
+        
+        let timeToCook = switch hardness {
+            case "Soft":
+                softTime
+            case "Medium":
+                mediumTime
+            case "Hard":
+                hardTime
+            default: 0
+        }
+        
+        print("Time to cook: \(timeToCook)")
     }
-
-
 }
 
