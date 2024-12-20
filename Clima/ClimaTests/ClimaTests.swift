b473ad1066da11e003f9fcb41229669f20c9d6ed
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2024-12-20 09:56:14 -0700

    Creación de proyecto

---
+++ Clima/ClimaTests/ClimaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaTests.swift
+//  ClimaTests
+//
+//  Created by Cesar Garduno on 12/20/24.
+//
+
+import Testing
+@testable import Clima
+
+struct ClimaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
